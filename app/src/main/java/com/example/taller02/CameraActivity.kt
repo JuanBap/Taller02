9a1307a29ae55a49aafadc28420e95b1591dfa5d
Juan Canon <124364006+JuanBap@users.noreply.github.com>
2024-09-26 15:27:25 -0500

    Punto 1 - Hecho

---
+++ app/src/main/java/com/example/taller02/CameraActivity.kt
@@ -0,0 +1,24 @@
+package com.example.taller02
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.taller02.databinding.ActivityCameraBinding
+import com.example.taller02.databinding.ActivityContactsBinding
+
+class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
--- app/src/main/java/com/example/taller02/MainActivity.kt
+++ app/src/main/java/com/example/taller02/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.taller02
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.taller02.databinding.ActivityMainBinding
+import org.w3c.dom.Attr
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonContacts.setOnClickListener{
+            startActivity(Intent(baseContext, ContactsActivity::class.java))
         }
+
+        binding.buttonCamera.setOnClickListener{
+            startActivity(Intent(baseContext, CameraActivity::class.java))
+        }
+
+        binding.buttonCamera.setOnClickListener{
+            startActivity(Intent(baseContext, CameraActivity::class.java))
+        }
+
     }
 }
